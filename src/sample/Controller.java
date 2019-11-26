76b71d5480f406168d600b2ea88e2f93ca19c3b9
Boldeusz <mnpower@poczta.onet.pl>
2019-11-26 16:04:11 +0100

    gggg

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -12,6 +13,8 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox checkBox;
 
     @FXML
     public void initialize(){
@@ -25,6 +28,16 @@ public class Controller {
         }else if (event.getSource().equals(byeButton)){
             System.out.println("Bye, " + nameField.getText());
         }
+        try{
+            Thread.sleep(10000);
+        }catch (InterruptedException e){
+
+        }
+        if (checkBox.isSelected()){
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
 
     }
     @FXML
@@ -33,6 +46,10 @@ public class Controller {
         boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
+    }
+    @FXML
+    public void handleChange(){
+        System.out.println("The checkbox is " + (checkBox.isSelected() ? "checked" : "not checked"));
     }
 
 }
